8df432f3c8fbda6f7932f487580ad55afc54b235
Joona Piirainen <joona.piirainen@gmail.com>
2021-03-17 19:36:05 +0200

    examples

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import * as M from "./monocle"
 
+const Book1: M.Book = {
+	published: 1969,
+	title: "Book 1",
+}
+const Book2: M.Book = {
+	published: 2969,
+	title: "Book 2",
+}
 const jeppe: M.Person = {
 	email: "jeppe@gmail.com",
 	favoriteColors: ["red"],
 	name: "jeppe",
+	address: {
+		postalCode: 25200,
+		streetName: "foobarStreet",
+		streetNr: 69,
+	},
+	favoriteBooks: [Book1, Book2],
 }
 
-console.log(M._name.get(jeppe))
+const jeppeNewName = M._email.modify(_ => "foobar")(jeppe)
 
-const f = M._email.modify(_ => "foobar")(jeppe)
-
-console.log(f)
+const updatePersonStreetName: (newName: string) => (oldP: M.Person) => M.Person = n => oldPerson =>
+	M._pStreetName.modify(_ => n)(oldPerson)
--- src/monocle/index.ts
+++ src/monocle/index.ts
@@ -1,11 +1,46 @@
-import { Lens } from "monocle-ts"
+import { Lens, fromTraversable } from "monocle-ts"
+import { array } from "fp-ts/Array"
 
 export interface Person {
 	readonly name: string
 	readonly email: string
 	readonly favoriteColors: Array<string>
+	readonly favoriteBooks: Array<Book>
+	readonly address: Address
 }
 
+export interface Book {
+	readonly title: string
+	readonly published: number
+}
+
+export interface Address {
+	readonly streetName: string
+	readonly streetNr: number
+	readonly postalCode: number
+}
+
+// ? person lenses
 export const _name = Lens.fromProp<Person>()("name")
 export const _email = Lens.fromProp<Person>()("email")
-export const _favColors = Lens.fromProp<Person>()("favoriteColors")
+export const _address = Lens.fromProp<Person>()("address")
+export const _books = Lens.fromProp<Person>()("favoriteBooks")
+export const _favoriteColors = Lens.fromProp<Person>()("favoriteColors")
+
+// ? address lenses
+export const _streenName = Lens.fromProp<Address>()("streetName")
+export const _streetNr = Lens.fromProp<Address>()("streetNr")
+export const _postalCode = Lens.fromProp<Address>()("postalCode")
+
+// ? book lenses
+export const _bookTitle = Lens.fromProp<Book>()("title")
+export const _bookPublished = Lens.fromProp<Book>()("published")
+
+// ? lens compositions
+export const _pStreetName = _address.compose(_streenName)
+export const _pStreetNr = _address.compose(_streetNr)
+export const _pPostalCode = _address.compose(_postalCode)
+
+// ? traversables
+export const _booksTra = fromTraversable(array)<Book>()
+export const _favoriteColorsTra = fromTraversable(array)<string>()
